65eb4f74e3d7e425329b7ebc4625b86437f52d69
Michael Coons <michael@Michaels-MacBook-Pro-3.local>
2018-07-06 13:05:16 -0600

    created base js API code

--- index.js
+++ index.js
@@ -55,3 +55,36 @@ wind.addEventListener("click", function(){
   wind.classList.add('wind');
   wind.classList.add('active');
 })
+
+
+let baseURI = "https://galvanize-cors.herokuapp.com/http://api.musixmatch.com/ws/1.1/"
+
+let fireTract = "track.search?apikey=19f997a50a7117eb12ee72fea784a8dc&q_track=Fire&page_size=10"
+
+let fireTracks = []
+
+document.getElementById("bottomleft").addEventListener("click", fireClick);
+
+function fireClick(event){
+fetch(
+  "https://galvanize-cors.herokuapp.com/http://api.musixmatch.com/ws/1.1/track.search?apikey=19f997a50a7117eb12ee72fea784a8dc&q_track=Fire&page_size=10")
+  .then(response => response.json())
+  .then(result => {fireTracks = result.message.body.track_list; return result;})
+  .then(result => {console.log(fireTracks); processData(fireTracks)});
+
+}
+  function processData(trackList){
+    trackList.forEach( element => createCard(element));
+  }
+
+  function createCard(element){
+
+    
+    let card = document.createElement("div");
+    card.classList.add("card");
+    card.innerText = `Track Name:  ${element.track.track_name} \n Artist Name: ${element.track.artist_name}`;
+
+    document.getElementById("bottomleft").appendChild(card);
+  }
+
+//http://api.musixmatch.com/ws/1.1/track.search?apikey=***&q_track=Dire%20straits%20Sultans%20of%20Swing&page_size=10
