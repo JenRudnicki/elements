b11ffbde2dfb6a07e7acf86030466b37d7bf0aea
JenRudnicki <jenny.rudnicki@comcast.net>
2018-07-06 13:39:17 -0600

    added water tracks

--- index.js
+++ index.js
@@ -75,24 +75,45 @@ fetch(
 
 //////////////////////////////////
 
+let waterTracks = []; 
+
+document.getElementById("bottomright").addEventListener("click", waterClick);
+
+function waterClick(event){
+
+console.log("In Water");
+
+fetch(
+  "https://galvanize-cors.herokuapp.com/http://api.musixmatch.com/ws/1.1/track.search?apikey=19f997a50a7117eb12ee72fea784a8dc&q_track=Water&page_size=10")
+  .then(response => response.json())
+  .then(result => {waterTracks = result.message.body.track_list; return result;})
+  .then(result => {console.log(waterTracks); processData(waterTracks,"bottomright")});
 
 
+}
 
 
-
-
-function processData(trackList){
-    trackList.forEach( element => createCard(element));
+function processData(trackList, location){
+    trackList.forEach( element => createCard(element,location));
   }
 
-function createCard(element){
+function createCard(element, location){
 
     
     let card = document.createElement("div");
     card.classList.add("card");
-    card.innerText = `Track Name:  ${element.track.track_name} \n Artist Name: ${element.track.artist_name} \n`;
+    let track = document.createElement("h1");
+    track.innerText = `Track Name:  ${element.track.track_name}`; 
+    let artist = document.createElement("h3");
+    artist.innerText = `Artist Name:  ${element.track.artist_name}`; 
 
-    document.getElementById("bottomleft").appendChild(card);
+    card.appendChild(track);
+    card.appendChild(artist);
+    
+
+    // card.innerText = `Track Name:  ${element.track.track_name} \n Artist Name: ${element.track.artist_name} \n`;
+
+    document.getElementById(location).appendChild(card);
   }
 
 //http://api.musixmatch.com/ws/1.1/track.search?apikey=***&q_track=Dire%20straits%20Sultans%20of%20Swing&page_size=10
