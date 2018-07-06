a36c10068ba9968b8fd3c0a37c359c2b8cb6535f
Michael Coons <michael@Michaels-MacBook-Pro-3.local>
2018-07-06 14:07:14 -0600

    working fire and water

--- index.js
+++ index.js
@@ -71,11 +71,27 @@ let fireTracks = [];  // duplicate
 document.getElementById("bottomleft").addEventListener("click", fireClick);
 
 function fireClick(event){
+  // let bl = document.querySelector("#bottomleft .cardContainer");
+  let br = document.querySelector("#bottomright .cardContainer");
+  let tl = document.querySelector("#topleft .cardContainer");
+  let tr = document.querySelector("#topright .cardContainer");
+
+
+  while (br.hasChildNodes()){ br.removeChild(br.lastChild);
+  }
+
+  while (tl.hasChildNodes()){ tl.removeChild(tl.lastChild);
+  }
+
+  while (tr.hasChildNodes()){ tr.removeChild(tr.lastChild);
+  }
+
+
 fetch(
   "https://galvanize-cors.herokuapp.com/http://api.musixmatch.com/ws/1.1/track.search?apikey=19f997a50a7117eb12ee72fea784a8dc&q_track=Fire&page_size=10")
   .then(response => response.json())
   .then(result => {fireTracks = result.message.body.track_list; return result;})
-  .then(result => {console.log(fireTracks); processData(fireTracks)});
+  .then(result => {console.log(fireTracks); processData(fireTracks, "#bottomleft .cardContainer")});
 }
 
 //////////////////////////////////
@@ -83,27 +99,24 @@ fetch(
 let waterTracks = []; 
 
 document.getElementById("bottomright").addEventListener("click", waterClick);
-let bl = document.getElementById("bottomleft");
-let tl = document.getElementById("topleft");
-let tr = document.getElementById("topright");
-
 
 
 
 function waterClick(event){
 
-  let bl = document.getElementById("bottomleft");
-  let tl = document.getElementById("topleft");
-  let tr = document.getElementById("topright");
+  let bl = document.querySelector("#bottomleft .cardContainer");
+  let tl = document.querySelector("#topleft .cardContainer");
+  let tr = document.querySelector("#topright .cardContainer");
 
-  // while (bl.hasChildNodes()){ bl.removeChild(bl.lastChild);
-  // }
 
-  // while (tl.hasChildNodes()){ tl.removeChild(tl.lastChild);
-  // }
+  while (bl.hasChildNodes()){ bl.removeChild(bl.lastChild);
+  }
 
-  // while (tr.hasChildNodes()){ tr.removeChild(tr.lastChild);
-  // }
+  while (tl.hasChildNodes()){ tl.removeChild(tl.lastChild);
+  }
+
+  while (tr.hasChildNodes()){ tr.removeChild(tr.lastChild);
+  }
 
 // console.log("In Water");
 
@@ -111,7 +124,7 @@ fetch(
   "https://galvanize-cors.herokuapp.com/http://api.musixmatch.com/ws/1.1/track.search?apikey=19f997a50a7117eb12ee72fea784a8dc&q_track=Water&page_size=10")
   .then(response => response.json())
   .then(result => {waterTracks = result.message.body.track_list; return result;})
-  .then(result => {console.log(waterTracks); processData(waterTracks,"bottomright")});
+  .then(result => {console.log(waterTracks); processData(waterTracks,"#bottomright .cardContainer")});
 
 
 }
@@ -137,7 +150,7 @@ function createCard(element, location){
 
     // card.innerText = `Track Name:  ${element.track.track_name} \n Artist Name: ${element.track.artist_name} \n`;
 
-    document.getElementById(location).appendChild(card);
+    document.querySelector(location).appendChild(card);
   }
 
 //http://api.musixmatch.com/ws/1.1/track.search?apikey=***&q_track=Dire%20straits%20Sultans%20of%20Swing&page_size=10
